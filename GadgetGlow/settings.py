35c3f9670b6bfa1e84c88e055b423fbfd179f3f2
Pratyush Kumar <kumarpratyush6294@gmail.com>
2024-05-01 19:13:49 -0400

    changed host oconfiguration

--- GadgetGlow/settings.py
+++ GadgetGlow/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.0.210']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
